d69ba8a55e00161e45040a6e6781b922f1766f91
Paulo Barata <pbara7a@gmail.com>
2022-06-15 18:03:37 +0100

    seeded the equipment and target table.

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,8 +1,39 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const exercises = require("../exercises.json");
+
 async function seed() {
+  const equipments = await createEquipments();
+  const bodyParts = await createTargets();
+
   process.exit(0);
+}
+
+async function createEquipments() {
+  const equipments = [];
+  exercises.forEach((exercise) => {
+    equipments.push({ name: exercise.equipment });
+  });
+
+  const createdEquipments = await prisma.equipment.createMany({
+    data: equipments,
+    skipDuplicates: true,
+  });
+  return createdEquipments;
+}
+
+async function createTargets() {
+  const targets = [];
+  exercises.forEach((exercise) => {
+    targets.push({ name: exercise.target });
+  });
+
+  const createdTargets = await prisma.target.createMany({
+    data: targets,
+    skipDuplicates: true,
+  });
+  return createdTargets;
 }
 
 seed()
