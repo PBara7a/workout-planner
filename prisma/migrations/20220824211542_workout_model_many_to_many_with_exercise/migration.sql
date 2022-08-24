c91fdfd983c6c5e8802d02dcce8509fe1c08ee80
Paulo Barata <pbara7a@gmail.com>
2022-08-24 22:16:17 +0100

    Workout model

---
+++ prisma/migrations/20220824211542_workout_model_many_to_many_with_exercise/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "Workout" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "target" TEXT,
+    "notes" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Workout_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ExerciseToWorkout" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Workout_name_key" ON "Workout"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Workout_target_key" ON "Workout"("target");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Workout_notes_key" ON "Workout"("notes");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ExerciseToWorkout_AB_unique" ON "_ExerciseToWorkout"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ExerciseToWorkout_B_index" ON "_ExerciseToWorkout"("B");
+
+-- AddForeignKey
+ALTER TABLE "_ExerciseToWorkout" ADD CONSTRAINT "_ExerciseToWorkout_A_fkey" FOREIGN KEY ("A") REFERENCES "Exercise"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ExerciseToWorkout" ADD CONSTRAINT "_ExerciseToWorkout_B_fkey" FOREIGN KEY ("B") REFERENCES "Workout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
